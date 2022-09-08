3d714c3e39d39e42fccafe020ea3d9adbf7bfc49
Polgar Kristof <polgarkristof10@gmail.com>
2022-09-08 12:44:41 +0200

    array Sort

---
+++ masodikfeladat.js
@@ -0,0 +1,8 @@
+function getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+let macskak = [];
+let macsk = {nev: "Cirmos ", eletkor: getRandomArbitrary(2,10)};
+for ( i = 0; i < 20 ; i++ ) {
+    macskak[i].push(})
+}
